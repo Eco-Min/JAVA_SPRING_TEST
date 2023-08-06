6bc2a3c9cd23bb8c39e04ef6f9577de907bab06b
eco_min <guldor10.b@gmail.com>
2023-08-06 13:22:44 +0900

    java/spring test UserRepositoryTest

---
+++ test-code-with-architecture-main/src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserStatus;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest(showSql = true)
+@TestPropertySource("classpath:application-test.yml")
+@Sql("/sql/user-repository-test-data.sql")
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void findByIdAndStatus() throws Exception{
+        // given
+
+        // when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+
+        // then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    public void findByIdAndStatus_empty_data() throws Exception{
+        // given
+
+        // when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+
+        // then
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void findByEmailAndStatus() throws Exception{
+        // given
+
+        // when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("eco@naver.com", UserStatus.ACTIVE);
+
+        // then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    public void findByEmailAndStatus_empty_data() throws Exception{
+        // given
+
+        // when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("eco@naver.com", UserStatus.PENDING);
+
+        // then
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+}
---
+++ test-code-with-architecture-main/src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'eco@naver.com', 'eco', 'Seoul', 'aaaaaaaaaaa-aaaa-aaaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
